03589fb4fd072c51239fc9f93ddb747f4812df4f
makskayunPC <maks.kayn@yandex.com>
2021-09-12 21:14:25 +0300

    init

---
+++ Arrays and lists/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arrays_and_lists
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            // Train train = new Train();
+            // train.Main();
+            
+            // CommonElements ce = new CommonElements();
+            // ce.Main();
+            
+            // BigNumbers bn = new BigNumbers();
+            // bn.Main();
+            
+            // RotationOfArray roa = new RotationOfArray();
+            // roa.Main();
+            
+            // MagicSum ms = new MagicSum();
+            // ms.Main();
+
+            // TrainWorkList twl = new TrainWorkList();
+            // twl.Main();
+
+            // ChangingList chlst = new ChangingList();
+            // chlst.Main();
+
+            // WorkingWithList wwlst = new WorkingWithList();
+            // wwlst.Main();
+        }
+    }
+}
